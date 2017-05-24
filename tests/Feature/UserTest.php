f30cdf59a1f66a5ebec06e2f8eb31c89af82f781
Serhii Solohub <zeitgeist@ukr.net>
2017-05-24 19:43:48 +0300

    using Mail facade instead of simply sendmail

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use Illuminate\Support\Facades\Mail;
+
 class UserTest extends TestCase
 {
 
